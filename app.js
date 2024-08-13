70100d93af9f3e2f523f992e04513c132ccdc282
hainweb <hainofficialweb@gmail.com>
2024-08-13 19:18:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
-app.use(session({secret:"key",cookie:{maxAge:600000}}))
+// app.use(session({secret:"key",cookie:{maxAge:600000}}))
+
+app.use(session({
+  secret: 'ajinajinshoppingsecretisajin',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: 'your-mongo-connection-string' }),
+  cookie:{maxAge:6000000}
+}));
 
 db.connect((err)=>{
   if (err)
